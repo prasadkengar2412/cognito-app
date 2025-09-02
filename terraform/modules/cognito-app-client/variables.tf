da562dee020ffb11fbf049510ba83fd496ee0fba
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-02 11:11:23 +0530

    Create variables.tf

---
+++ terraform/modules/cognito-app-client/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type = string
+}
+variable "application_name" {
+  type = string
+}
+variable "env" {
+  type = string
+}
+variable "redirect_urls" {
+  type = list(string)
+}
+variable "logout_urls" {
+  type = list(string)
+}
+variable "scopes" {
+  type = list(string)
+}
+variable "custom_scopes" {
+  type    = list(string)
+  default = []
+}
+variable "branding_settings_path" {
+  type = string
+}
+variable "branding_assets_path" {
+  type = string
+}
