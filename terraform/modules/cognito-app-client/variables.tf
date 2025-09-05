534e653b87687265eb9511a01da3c01e484e08c6
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 12:29:35 +0530

    Update variables.tf

--- terraform/modules/cognito-app-client/variables.tf
+++ terraform/modules/cognito-app-client/variables.tf
@@ -1,9 +1,49 @@
 variable "region" {
-  type    = string
-  default = "us-east-1"
+  type = string
 }
-
+variable "application_name" {
+  type = string
+}
 variable "env" {
-  type        = string
-  description = "Environment (dev, stg, prod)"
+  type = string
+}
+variable "client_type" {
+  type = string
+}
+variable "redirect_urls" {
+  type = list(string)
+}
+variable "logout_urls" {
+  type = list(string)
+}
+variable "scopes" {
+  type = list(string)
+}
+variable "custom_scopes" {
+  type    = list(string)
+  default = []
+}
+variable "branding_settings_path" {
+  type = string
+}
+variable "branding_assets_path" {
+  type = string
+}
+variable "access_token_validity" {
+  type = object({
+    value = number
+    unit  = string
+  })
+}
+variable "id_token_validity" {
+  type = object({
+    value = number
+    unit  = string
+  })
+}
+variable "refresh_token_validity" {
+  type = object({
+    value = number
+    unit  = string
+  })
 }
