a347df24db968ba725b2da617f0ae2600f149b24
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 21:39:41 +0530

    Update outputs.tf

--- terraform/modules/cognito-app-client/outputs.tf
+++ terraform/modules/cognito-app-client/outputs.tf
@@ -1,20 +1,7 @@
 output "client_id" {
-  value       = aws_cognito_user_pool_client.app_client.id
-  description = "Cognito app client ID"
+  value = aws_cognito_user_pool_client.app_client.id
 }
 
 output "secret_arn" {
-  value       = aws_secretsmanager_secret.app_secret.arn
-  description = "Secrets Manager ARN"
-}
-
-output "branding_files_used" {
-  value = local.apply_branding ? {
-    settings = var.branding_settings_path
-    assets   = var.branding_assets_path
-  } : {
-    settings = ""
-    assets   = ""
-    message  = "No branding files applied"
-  }
+  value = aws_secretsmanager_secret.app_secret.arn
 }
