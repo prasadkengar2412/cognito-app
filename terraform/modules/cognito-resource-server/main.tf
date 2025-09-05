05b5d48104a180903ba56e530b0b9f9a28ef9440
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 20:06:48 +0530

    Create main.tf

---
+++ terraform/modules/cognito-resource-server/main.tf
@@ -0,0 +1,22 @@
+data "aws_ssm_parameter" "user_pool_id" {
+  name            = "/${var.env == "dev" ? "development" : var.env == "stg" ? "staging" : "production"}/ULNG/UserPoolId"
+  with_decryption = true
+}
+
+resource "aws_cognito_resource_server" "this" {
+  user_pool_id = data.aws_ssm_parameter.user_pool_id.value
+  identifier   = var.identifier
+  name         = var.name
+
+  dynamic "scope" {
+    for_each = var.scopes
+    content {
+      scope_name        = scope.value
+      scope_description = "Scope for ${var.name} - ${scope.value}"
+    }
+  }
+}
+
+output "scopes" {
+  value = [for s in var.scopes : s]
+}
