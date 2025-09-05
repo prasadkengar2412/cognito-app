9ab50c5fa3c93f335750a3f3aa9f256220f9d14b
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 21:21:27 +0530

    Update variables.tf

--- terraform/modules/cognito-app-client/variables.tf
+++ terraform/modules/cognito-app-client/variables.tf
@@ -1,63 +1,13 @@
-variable "region" {
-  type = string
-}
-variable "application_name" {
-  type = string
-}
-variable "env" {
-  type = string
-}
-variable "client_type" {
-  type = string
-}
-variable "redirect_urls" {
-  type = list(string)
-}
-variable "logout_urls" {
-  type = list(string)
-}
-variable "scopes" {
-  type = list(string)
-}
-variable "custom_scopes" {
-  type    = list(string)
-  default = []
-}
-
-# NEW optional inputs for resource server override
-variable "resource_server_identifier" {
-  type    = string
-  default = ""
-}
-
-variable "resource_server_name" {
-  type    = string
-  default = ""
-}
-
-variable "branding_settings_path" {
-  type    = string
-  default = ""
-}
-variable "branding_assets_path" {
-  type    = string
-  default = ""
-}
-variable "access_token_validity" {
-  type = object({
-    value = number
-    unit  = string
-  })
-}
-variable "id_token_validity" {
-  type = object({
-    value = number
-    unit  = string
-  })
-}
-variable "refresh_token_validity" {
-  type = object({
-    value = number
-    unit  = string
-  })
-}
+variable "region" { type = string }
+variable "env" { type = string }
+variable "application_name" { type = string }
+variable "client_type" { type = string }
+variable "redirect_urls" { type = list(string) }
+variable "logout_urls" { type = list(string) }
+variable "scopes" { type = list(string) }
+variable "custom_scopes" { type = list(string), default = [] }
+variable "branding_settings_path" { type = string, default = "" }
+variable "branding_assets_path" { type = string, default = "" }
+variable "access_token_validity" { type = object({ value = number, unit = string }) }
+variable "id_token_validity" { type = object({ value = number, unit = string }) }
+variable "refresh_token_validity" { type = object({ value = number, unit = string }) }
