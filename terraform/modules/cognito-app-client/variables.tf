319066fa30df1aa3a7aa7ad9a7919827c79d3b73
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 21:39:23 +0530

    Update variables.tf

--- terraform/modules/cognito-app-client/variables.tf
+++ terraform/modules/cognito-app-client/variables.tf
@@ -1,13 +1,56 @@
-variable "region" { type = string }
-variable "env" { type = string }
-variable "application_name" { type = string }
-variable "client_type" { type = string }
-variable "redirect_urls" { type = list(string) }
-variable "logout_urls" { type = list(string) }
-variable "scopes" { type = list(string) }
-variable "custom_scopes" { type = list(string), default = [] }
-variable "branding_settings_path" { type = string, default = "" }
-variable "branding_assets_path" { type = string, default = "" }
-variable "access_token_validity" { type = object({ value = number, unit = string }) }
-variable "id_token_validity" { type = object({ value = number, unit = string }) }
-variable "refresh_token_validity" { type = object({ value = number, unit = string }) }
+variable "region" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "application_name" {
+  type = string
+}
+
+variable "client_type" {
+  type = string
+}
+
+variable "redirect_urls" {
+  type = list(string)
+}
+
+variable "logout_urls" {
+  type = list(string)
+}
+
+variable "scopes" {
+  type = list(string)
+  default = []
+}
+
+variable "custom_scopes" {
+  type = list(string)
+  default = []
+}
+
+variable "access_token_validity" {
+  type = object({ value = number, unit = string })
+}
+
+variable "id_token_validity" {
+  type = object({ value = number, unit = string })
+}
+
+variable "refresh_token_validity" {
+  type = object({ value = number, unit = string })
+}
+
+variable "branding_settings_path" {
+  type    = string
+  default = ""
+}
+
+variable "branding_assets_path" {
+  type    = string
+  default = ""
+}
+
