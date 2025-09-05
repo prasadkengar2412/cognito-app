cd194991b0642b3cc50a6f3d52dcf79fa95604b0
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-05 21:37:00 +0530

    Update main.tf

--- terraform/modules/cognito-resource-server/main.tf
+++ terraform/modules/cognito-resource-server/main.tf
@@ -1,30 +1,21 @@
+# Fetch User Pool ID from SSM Parameter Store
 data "aws_ssm_parameter" "user_pool_id" {
   name            = "/${var.env == "dev" ? "development" : var.env == "stg" ? "staging" : "production"}/ULNG/UserPoolId"
   with_decryption = true
 }
 
+# Create Resource Server and all its custom scopes
 resource "aws_cognito_resource_server" "this" {
+  for_each     = { for rs in var.resource_servers : rs.identifier => rs }
   user_pool_id = data.aws_ssm_parameter.user_pool_id.value
-  identifier   = var.identifier
-  name         = var.name
+  identifier   = each.value.identifier
+  name         = each.value.name
 
   dynamic "scope" {
-    for_each = var.scopes
+    for_each = each.value.scopes
     content {
       scope_name        = scope.value
-      scope_description = "Scope for ${var.name} - ${scope.value}"
+      scope_description = "Scope ${scope.value} for ${each.value.name}"
     }
   }
-}
-
-output "scopes" {
-  value = [for s in var.scopes : "${var.identifier}/${s}"]
-}
-
-output "identifier" {
-  value = var.identifier
-}
-
-output "name" {
-  value = var.name
 }
