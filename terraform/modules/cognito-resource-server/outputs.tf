a5d6d2acc0e1e339bed9c13e731e6a7ca72cd1ca
prasadkengar2412 <109298995+prasadkengar2412@users.noreply.github.com>
2025-09-15 14:35:31 +0530

    Update outputs.tf

--- terraform/modules/cognito-resource-server/outputs.tf
+++ terraform/modules/cognito-resource-server/outputs.tf
@@ -1,3 +1,7 @@
 output "resource_servers_map" {
   value = { for k, v in aws_cognito_resource_server.this : k => v.identifier }
 }
+output "has_resource_servers" {
+  description = "Boolean indicating if resource servers were created"
+  value       = length(aws_cognito_resource_server.this) > 0
+}
